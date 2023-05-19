7bd416ad2bf807ec9e0734e83bf028150e197e37
Atharva Pise <atharvapise19@gmail.com>
2023-05-19 21:30:51 +0530

    update astro-blog site config link

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://astro-blog-pita.app",
+  site: "https://astro-blog-pita.netlify.app",
   integrations: [
     image({
       serviceEntryPoint: "@astrojs/image/sharp",
